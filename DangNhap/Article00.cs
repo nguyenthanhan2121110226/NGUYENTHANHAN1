5df1261dfee6027a951f1c4c219bf69dc73118ec
nguyenthanhan2121110226 <139724665+nguyenthanhan2121110226@users.noreply.github.com>
2023-07-17 09:40:04 +0700

    Add files via upload

---
+++ DangNhap/Article00.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DangNhap
+{
+    public partial class Article00 : Form
+    {
+        public Article00()
+        {
+            InitializeComponent();
+            this.Load += Article00_Load;
+            this.ResizeEnd += Article00_ResizeEnd;
+        }
+        private void Article00_ResizeEnd(object sender, System.EventArgs e)
+        {
+            int width = this.Size.Width;
+            int height = this.Size.Height;
+            this.Text = width.ToString() + " - " + height.ToString();
+        }
+
+        private void Article00_Load(object sender, System.EventArgs e)
+        {
+            int width = this.Size.Width;
+            int height = this.Size.Height;
+            this.Text = width.ToString() + " - " + height.ToString();
+
+        }
+
+
+    }
+}
---
+++ DangNhap/Article01.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Article01
+{
+    public partial class Form1 : Form
+    {
+        string path = @"D:\form.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Write(InforWindowns iw)
+        {
+            XmlSerializer write = new XmlSerializer(typeof(InforWindowns));
+            StreamWriter file = new StreamWriter(path);
+            write.Serialize(file, iw);
+            file.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InforWindowns iw = new InforWindowns();
+            iw.With = this.Size.Width;
+            iw.Height = this.Size.Height;
+            Write(iw);
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            InforWindowns iw = new InforWindowns();
+            iw.With = this.Size.Width;
+            iw.Height = this.Size.Height;
+            Write(iw);
+        }
+    }
+}
